df921f69a7ff747610c5237b30b972a22d9d8bfe
Ville Takala <ville.i.takala@gmail.com>
2020-07-12 07:18:44 +0300

    Revert "cleanup"
    This reverts commit cb98d8a9518f37a82f4472b4a440c23213785912.

---
+++ .storybook/main.js
@@ -0,0 +1,8 @@
+module.exports = {
+    addons: [
+        "@storybook/addon-actions",
+        "@storybook/addon-knobs/register",
+        "@storybook/addon-notes/register",
+        "@storybook/addon-viewport/register",
+    ],
+};
