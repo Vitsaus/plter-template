e501b27dcd1e1aae67529f99cae2dc4be7564d1c
Ville Takala <ville.i.takala@gmail.com>
2020-07-12 07:24:23 +0300

    Revert "Revert "cleanup""
    This reverts commit df921f69a7ff747610c5237b30b972a22d9d8bfe.

--- packages/ui/webpack.config.production.js
+++ packages/ui/webpack.config.production.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const ForkTsCheckerNotifierWebpackPlugin = require('fork-ts-checker-notifier-webpack-plugin');
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
 module.exports = {
