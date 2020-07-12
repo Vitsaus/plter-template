cb98d8a9518f37a82f4472b4a440c23213785912
Ville Takala <ville.i.takala@gmail.com>
2020-07-12 07:08:14 +0300

    cleanup

--- .storybook/main.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    addons: [
-        "@storybook/addon-actions",
-        "@storybook/addon-knobs/register",
-        "@storybook/addon-notes/register",
-        "@storybook/addon-viewport/register",
-    ],
-};
